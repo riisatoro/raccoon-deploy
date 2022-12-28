10d56d29a9addc654f05f791a373133fed7485e1
Yaroslav Luzin <jardev@gmail.com>
2022-12-28 13:52:00 +0200

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref, onMounted} from "vue";
+const status = ref(null);
+onMounted(async () => {
+  const response = await fetch("/health");
+  status.value = response.status;
+});
+</script>
+
+<template>
+  <div class="flex h-screen items-center justify-center bg-indigo-50">
+    <div
+      class="rounded-full border p-5 shadow"
+      :class="{'bg-gray-500': status === null, 'bg-green-500': status === 200, 'bg-red-500': status !== 200}"
+    >
+      {{ status }}
+    </div>
+  </div>
+</template>
